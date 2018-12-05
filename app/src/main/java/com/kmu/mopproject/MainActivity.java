1a91ed210a00f25053635ba705f69298efd5c66f
Juyeong <jjy8604@gmail.com>
2018-12-05 22:34:11 +0900

    화면 전환 구현

--- app/src/main/java/com/kmu/mopproject/MainActivity.java
+++ app/src/main/java/com/kmu/mopproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kmu.mopproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,14 +81,24 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        if (id == R.id.nav_special_day) {
+//            item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+//                @Override
+//                public boolean onMenuItemClick(MenuItem item) {
+//                    return true;
+//                }
+//            });
+            Intent intent = new Intent(MainActivity.this, special_day.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_travel) {
+            Intent intent=new Intent(MainActivity.this,travel.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_foods_pic) {
+            Intent intent=new Intent(MainActivity.this,foods_pic.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_bucket_list) {
+            Intent intent=new Intent(MainActivity.this,bucket_list.class);
+            startActivity(intent);
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
